a7556dec3d15718687ffd80f2d87d216eb15464b
rwu <ruichao.wu@ipa.fraunhofer.de>
2020-11-11 10:37:46 +0100

    first commit

---
+++ scripts/client_execution.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from action_client.client import MinimalActionClient
+import rclpy
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    action_client = MinimalActionClient()
+
+    action_client.send_goal()
+
+    rclpy.spin(action_client)
+
+
+if __name__ == '__main__':
+    main()
